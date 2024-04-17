e1e496faca33e3c3fe720008a2c6e392cd253923
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-04-17 12:45:20 +0200

    Initial commit

---
+++ src/pages/SellerRegistrationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellerRegistrationPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SellerRegistrationPage;
